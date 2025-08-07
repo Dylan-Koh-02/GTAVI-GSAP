dbb970cf80d01651bdd9eedf453acbdfda62da3d
Dylan <zikangkoh0113@gmail.com>
2025-08-07 12:55:08 +0800

    Hero Section CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl text-pink">Welcome to the world of GTA and GSAP!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
